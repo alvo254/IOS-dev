005aaeaf1e595355795c2b989c29073cc016ad51
Alvin <alvinkhamron@gmail.com>
2020-08-11 12:35:13 +0300

    Initial Commit

---
+++ loginFireBase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  loginFireBase
+//
+//  Created by User on 11/08/2020.
+//  Copyright © 2020 Alvin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
